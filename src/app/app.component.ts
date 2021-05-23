d23c89481f921be4740416177512de419489e56a
sokoo78 <sokoo78@gmail.com>
2021-05-23 16:59:48 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'bookstore-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  pageTitle: string = 'Bookstore';
+}
