16cc46bc44ba3a37abdfb66ccc8a8f252a6df703
sokoo78 <sokoo78@gmail.com>
2021-05-24 16:45:33 +0200

    1 file modified

--- src/app/books/book-list.component.ts
+++ src/app/books/book-list.component.ts
@@ -1,16 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { BookService } from '../book.service';
+import { Subscription } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'bookstore-books',
   templateUrl: './book-list.component.html'
 })
-export class BookListComponent implements OnInit {
-  books: any;
-
+export class BookListComponent implements OnInit, OnDestroy {
   constructor(private bookService: BookService) {}
 
+  books: any[] = [];
+  booksSub: Subscription;
+
   ngOnInit() {
-    this.bookService.getBooks().subscribe(result => (this.books = result));
+    this.booksSub = this.bookService
+      .getBooks()
+      .subscribe(result => (this.books = result));
+  }
+
+  ngOnDestroy() {
+    if (this.booksSub) this.booksSub.unsubscribe();
+  }
+
+  onDeleteBook(bookId: number): void {
+    this.bookService
+      .deleteBook(bookId)
+      .pipe(switchMap(res => this.bookService.getBooks()))
+      .subscribe(result => (this.books = result));
   }
 }
