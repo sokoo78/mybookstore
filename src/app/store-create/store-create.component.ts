41703161d7018b257faa9bcdaa54a119fd5dac4b
sokoo78 <sokoo78@gmail.com>
2021-05-28 14:01:50 +0200

    1 file modified, 1 deleted

--- src/app/store-create/store-create.component.ts
+++ src/app/store-create/store-create.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthorService } from '../author.service';
+import { Author } from '../authors/author';
+import { StoreService } from '../store.service';
 
 @Component({
   selector: 'app-store-create',
-  templateUrl: './store-create.component.html',
-  styleUrls: ['./store-create.component.css']
+  templateUrl: './store-create.component.html'
 })
 export class StoreCreateComponent implements OnInit {
+  storeForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private storeService: StoreService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    this.storeForm = this.formBuilder.group({
+      name: '',
+      place: '',
+      zipcode: '',
+      address: '',
+      openinghours: ''
+    });
   }
 
+  onSubmit(storeData) {
+    storeData.books = [];
+    storeData.amounts = [];
+    this.storeService.createStore(storeData).subscribe(() => {
+      this.storeForm.reset();
+      this.router.navigate(['/stores']);
+    });
+  }
 }
