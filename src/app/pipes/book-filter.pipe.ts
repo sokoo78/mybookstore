ebb1766c658ea86327081723ab4e1e251e55e2d5
sokoo78 <sokoo78@gmail.com>
2021-05-28 11:54:00 +0200

    1 file modified, 1 added

---
+++ src/app/pipes/book-filter.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'bookFilter'
+})
+export class BookFilterPipe implements PipeTransform {
+
+  transform(value: any, args?: any): any {
+    return null;
+  }
+
+}
